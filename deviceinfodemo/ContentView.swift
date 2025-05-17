f25ae21a834c17e9175608c18633c53ec3188e1f
Johann Petzold <johann.petzold@gmail.com>
2025-05-17 15:22:56 +0200

    Initial Commit

---
+++ deviceinfodemo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  deviceinfodemo
+//
+//  Created by Johann Petzold on 17/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ deviceinfodemo/deviceinfodemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  deviceinfodemoApp.swift
+//  deviceinfodemo
+//
+//  Created by Johann Petzold on 17/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct deviceinfodemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
